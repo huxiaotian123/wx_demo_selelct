f8eff4349a36caf9835118e2a0dac4b1f496d7f9
xiaotian <491494883@qq.com>
2017-01-06 00:48:04 +0800

    second

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,71 +1,73 @@
+var util = require('../../utils/util.js')
 
 
 var params = {
   data: {
-    isShow:false,
-    item_list:[],
+    isShow: false,
+    item_list: [],
+    first_index: -1,//第一层的选项
     array: [{
       message: '区域',
-      list:[
+      list: [
         {
-          dicNmae:"区域1",  
+          dicNmae: "区域1",
         },
-         {
-          dicNmae:"区域2",  
-        }, 
         {
-          dicNmae:"区域3",  
-        }, 
+          dicNmae: "区域2",
+        },
         {
-          dicNmae:"区域4",  
+          dicNmae: "区域3",
+        },
+        {
+          dicNmae: "区域4",
         }
       ]
     }, {
       message: '房型',
-      list:[
+      list: [
         {
-          dicNmae:"房型1",  
+          dicNmae: "房型1",
         },
-         {
-          dicNmae:"房型2",  
-        }, 
         {
-          dicNmae:"房型3",  
-        }, 
+          dicNmae: "房型2",
+        },
         {
-          dicNmae:"房型4",  
+          dicNmae: "房型3",
+        },
+        {
+          dicNmae: "房型4",
         }]
     },
     {
       message: '租金',
-      list:[
+      list: [
         {
-          dicNmae:"租金1",  
+          dicNmae: "租金1",
         },
-         {
-          dicNmae:"租金2",  
-        }, 
         {
-          dicNmae:"租金3",  
-        }, 
+          dicNmae: "租金2",
+        },
         {
-          dicNmae:"租金4",  
+          dicNmae: "租金3",
+        },
+        {
+          dicNmae: "租金4",
         }]
     },
     {
       message: '更多',
-      list:[
+      list: [
         {
-          dicNmae:"更多1",  
+          dicNmae: "更多1",
         },
-         {
-          dicNmae:"更多2",  
-        }, 
         {
-          dicNmae:"更多3",  
-        }, 
+          dicNmae: "更多2",
+        },
         {
-          dicNmae:"更多4",  
+          dicNmae: "更多3",
+        },
+        {
+          dicNmae: "更多4",
         }]
     }
     ]
@@ -75,23 +77,39 @@ var params = {
   ,
   expandList: function (event) {
     console.log(event)
-    
-    console.log(params.data.array);
-    wx.showToast({
-      title: params.data.array[event.target.id].message,
-      icon: 'success',
-      duration: 2000
-    })
 
-     
-       var array0 = params.data.array;
-       this.setData({isShow:true,item_list: params.data.array[event.target.id].list});
-       
-      // this.Page(params)
+    console.log(params.data.array);
+
+    var index = event.target.id;
+
+    if ( params.data.first_index == index && params.data.isShow) {
+      params.data.first_index = -1;
+      this.setData({ isShow: false, item_list: [] });
+    } else {
+      params.data.isShow = true;
+      params.data.first_index = index;
+      this.setData({ isShow: true, item_list: params.data.array[index].list });
+    }
+
+
+
+    // this.Page(params)
   }
+
+
+
 
 
 }
 
 
 Page(params)
+
+
+function showSuccessToast(str) {
+  wx.showToast({
+    title: str,
+    icon: 'success',
+    duration: 2000
+  })
+}
